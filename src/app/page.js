549c348acfa38078c210f2452746b4bdc60b3c5a
sathwik-234 <12213095@nitkkr.ac.in>
2024-12-15 10:30:48 +0530

    modified front page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import "..home/home.css"
 
 function page() {
   return (
-    <div>home</div>
-  )
+    <div className="home">
+       <div className="middle">
+               <div className="img-container">
+                   <img src="/home-1.png"></img>
+               </div>
+               <div className="text-container">
+                   <div className="Heading">
+                       SIMHACHALAM-NORTH
+                   </div>
+                   <div className="context">
+                           It is a "B" cateogry running room having  50 beds capacity located near to Gopalapatnam Railway Station.
+                   </div>
+               </div>
+           </div>
+   </div>
+)
 }
 
 export default page
