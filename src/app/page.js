66630ed8d03f7c83b9331a516c4393b39797a81a
sathwik-234 <12213095@nitkkr.ac.in>
2024-12-20 16:50:42 +0530

    yes

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,47 @@
 "use client"
-import React from 'react'
-import "./styles/home.css"
-import { useRouter } from 'next/navigation'
+import React from 'react';
+import "./home.css";
+import { useRouter } from 'next/navigation';
 
-function page() {
+const Page = () => {
     const nav = useRouter();
-  return (
-    <div className="home">
-       <div className="middle">
-               <div className="img-container">
-                   <img src="/home-1.png"></img>
-               </div>
-               <div className="text-container">
-                   <div className="Heading">
-                       SIMHACHALAM-NORTH
-                   </div>
-                   <div className="context">
-                           It is a "B" cateogry running room having  50 beds capacity located near to Gopalapatnam Railway Station.
-                   </div>
-                   <div className='signature-footer'>
-                   <p>-done by <span className="author">"G.Pradeep Kumar"</span></p>
-                   </div>
-               </div>
-           </div>
-   </div>
-)
-}
 
-export default page
+    return (
+        <div className="home">
+            <div className="middle">
+                <div className="img-container">
+                    <img src="/home-1.png" alt="Home"></img>
+                </div>
+                <div className="text-container">
+                    <div className="Heading">
+                        SIMHACHALAM-NORTH
+                    </div>
+                    <div className="context">
+                        It is a "B" category running room having 50 beds capacity located near to Gopalapatnam Railway Station.
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className="buttons-container">
+                        <button 
+                            className="button check-in" 
+                            onClick={() => nav.push("/CheckIn")}
+                        >
+                            CheckIn
+                        </button>
+                        <button 
+                            className="button check-out" 
+                            onClick={() => nav.push("/CheckOut")}
+                        >
+                            CheckOut
+                        </button>
+                    </div>
+                    <div className='signature-footer'>
+                        <p>-done by <span className="author">"G.Pradeep Kumar"</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
