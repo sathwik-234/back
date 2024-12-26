5563101870937dda8aaeccbc773f26d55cf93826
sathwik-234 <12213095@nitkkr.ac.in>
2024-12-26 07:54:30 +0530

    update About Us section with refined introduction and adjusted team member name formatting

--- src/app/AboutUs/page.js
+++ src/app/AboutUs/page.js
@@ -9,7 +9,6 @@ function AboutUs() {
         <section className="mission-section">
           <h2 >About This Application</h2>
           <p>
-          Welcome to the official Room Booking Platform of the East Coast Railway, Government of India!
           Welcome to the Room Booking Platform for Running Rooms, developed by East Coast Railway, Waltair Division. This platform is designed to offer a seamless and efficient lodging experience for crew members and officials. It streamlines the booking process while catering to their specific needs.
 
 Equipped with advanced functionalities, the system enables real-time monitoring of bed availability and minimizes waiting times, ensuring timely rest for the crew. Transitioning from manual calculations, the software now proficiently automates key metrics, including crew counts by HQ, running room rest durations, meal consumption, bedsheet and blanket utilization, and peak occupancy analysis for any specified period.
@@ -52,7 +51,7 @@ Equipped with advanced functionalities, the system enables real-time monitoring 
                 alt="Team Member"
                 className="team-photo"
               />
-              <h3>Sanchay Adari</h3>
+              <h3>Sanchay    Adari</h3>
               <p>DEE(OP)</p>
             </div>
             <div className="team-member">
