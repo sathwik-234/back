af3b13e79234e08afb755cc4b4fd4e029d0cf508
sathwik-234 <12213095@nitkkr.ac.in>
2025-01-06 16:15:56 +0530

    refactor: replace Thank You page with a new design and styles for improved user experience

---
+++ src/app/(extra)/ThankYou/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./thanku.css"
+
+
+function ThankYouPage() {
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="heading">Thank You!</h1>
+        <p className="message">
+          We're grateful for your interest. Please make sure to log in from your current to get started.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+// Inline styling
+
+export default ThankYouPage;
--- src/app/(frontend)/ThankYou/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>ThankYou</div>
-  )
-}
-
-export default page
